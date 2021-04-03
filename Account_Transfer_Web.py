00927996ddc34b465a39ea2fda29d3a50619d49a
isunyoo <isunyoo@gmail.com>
2021-04-03 15:47:11 +0800

    Updated

--- Account_Transfer_Web.py
+++ Account_Transfer_Web.py
@@ -2,7 +2,7 @@ import json, binascii, requests, glob
 from web3 import Web3
 from web3.auto import w3
 from decouple import config
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash, Markup
 
 ganache_url = "HTTP://127.0.0.1:8545"
 web3 = Web3(Web3.HTTPProvider(ganache_url))
@@ -21,7 +21,6 @@ _global_wallet_addresses = []
 _global_wallet_balances = []
 _global_wallet_balance_ether = []
 _global_wallet_balance_usd = []
-_global_addresses_cipher = []
 _recipient_wallet_addresses = []
 _recipient_wallet_balance_ether = []
 _recipient_wallet_balance_usd = []
@@ -29,7 +28,6 @@ _recipient_wallet_balance_usd = []
 
 # Get the current price of cryptocurrency conversion API URL
 req = requests.get(API_URL)
-# print(json.loads(req.content))
 USD_CURRENCY=json.loads(req.content)["USD"]
 
 
@@ -79,7 +77,6 @@ def extractAccounts():
 
     # clearing the lists
     _global_wallet_addresses.clear()            
-    _global_addresses_cipher.clear()
     _global_wallet_balances.clear()            
     _global_wallet_balance_ether.clear()       
     _global_wallet_balance_usd.clear()          
@@ -89,22 +86,17 @@ def extractAccounts():
         with open(single_file, 'r') as sf:            
             json_file = json.load(sf)                 
             global _global_wallet_address_counts                   
-            _global_wallet_addresses.insert(idx, web3.toChecksumAddress(json_file["address"]))            
-            _global_addresses_cipher.insert(idx, json_file["crypto"]["ciphertext"])            
-            # print("Account_Cipher :", _global_addresses_cipher[idx])
+            _global_wallet_addresses.insert(idx, web3.toChecksumAddress(json_file["address"]))                        
             _global_wallet_address_counts += 1
-            sf.close()
-                        
+            sf.close()                        
         with open(single_file, 'r') as keyfile: 
             _encrypted_key = keyfile.read()             
             _global_wallet_balances.insert(idx, web3.eth.getBalance(_global_wallet_addresses[idx]))
             _global_wallet_balance_ether.insert(idx, str(toEther(_global_wallet_balances[idx])))
-            _global_wallet_balance_usd.insert(idx, toUSD(_global_wallet_balances[idx]))
-            # print("Private_Key :", binascii.b2a_hex(w3.eth.account.decrypt(_encrypted_key, ACCOUNT_KEY)).decode('ascii'))                     
-            # print("Ether Balance :", toEther(_global_wallet_balances[idx]),"ETH =",toUSD(_global_wallet_balances[idx]),"$USD\n")     
+            _global_wallet_balance_usd.insert(idx, toUSD(_global_wallet_balances[idx]))                      
             keyfile.close()
     
-    return _global_wallet_addresses, _global_addresses_cipher, _global_wallet_balance_ether, _global_wallet_balance_usd
+    return _global_wallet_addresses, _global_wallet_balance_ether, _global_wallet_balance_usd
 
 
 # Function to extract principal account cipher
@@ -121,43 +113,15 @@ def extractPrincipalCipher(principalAddress):
     for idx, single_file in enumerate(files):
         with open(single_file, 'r') as sf:            
             json_file = json.load(sf)                                             
-            _local_principal_addresses.insert(idx, web3.toChecksumAddress(json_file["address"]))            
-            _local_principal_addresses_cipher.insert(idx, json_file["crypto"]["ciphertext"])                        
+            _local_principal_addresses.insert(idx, web3.toChecksumAddress(json_file["address"]))                                              
             sf.close()
+        with open(single_file, 'r') as keyfile: 
+            _encrypted_key = keyfile.read()                         
+            _local_principal_addresses_cipher.insert(idx, binascii.b2a_hex(w3.eth.account.decrypt(_encrypted_key, ACCOUNT_KEY)).decode('ascii'))                        
+            keyfile.close()
         if(str(principalAddress) == str(_local_principal_addresses[idx])): _principal_addresses_cipher = _local_principal_addresses_cipher[idx]            
         
     return _principal_addresses_cipher
-
-
-# Function to select principle account
-def selectPrincipalAccount(): 
-    # Change the glob if you want to only look through files with specific names
-    files = glob.glob(f'{KFILE_HOME}/*', recursive=True)
-    # Define the principle address   
-    global _global_principal_address, _global_decrypted_key
-    _principal_Address = []    
-    _principal_private_key = []
-    sen_num = input("\nSelect your principle address[#]: ")            
-    # Loop through multiple files        
-    if((int(sen_num) > 0) and (int(sen_num) <= _global_wallet_address_counts)):                
-        for idx, single_file in enumerate(files):
-            with open(single_file, 'r') as sf:           
-                json_file = json.load(sf)                     
-                _principal_Address.insert(idx, web3.toChecksumAddress(json_file["address"]))                        
-                sf.close()
-
-                with open(single_file, 'r') as keyfile:        
-                    _encrypted_key = keyfile.read()                    
-                    _principal_private_key.insert(idx, w3.eth.account.decrypt(_encrypted_key, ACCOUNT_KEY))                                                             
-                    keyfile.close()                            
-
-        _global_principal_address = _principal_Address[int(sen_num)-1]
-        print(f"Selected Principal Account Address : {_principal_Address[int(sen_num)-1]}\n")        
-        _global_decrypted_key = _principal_private_key[int(sen_num)-1]
-        # print(f"Selected Principal Private Binary Key : {_principal_private_key[int(sen_num)-1]}\n")        
-    else:            
-        print('Wrong Principle Account Selection.')  
-        exit()    
 
 
 # Fucntion to calling account and printing all accounts' balances
@@ -170,52 +134,12 @@ def listAccounts():
         _recipient_wallet_addresses.insert(idx, account)
         _recipient_wallet_balance_ether.insert(idx, str(toEther(web3.eth.getBalance(account))))
         _recipient_wallet_balance_usd.insert(idx, toUSD(web3.eth.getBalance(account)))
-        print(f'[{idx+1}] Balance of {account} : {toEther(web3.eth.getBalance(account))} ETH')
+        # print(f'[{idx+1}] Balance of {account} : {toEther(web3.eth.getBalance(account))} ETH')
 
     return _recipient_wallet_addresses, _recipient_wallet_balance_ether, _recipient_wallet_balance_usd
 
 
-# Function to transfer console ethereum
-def sendEther(rec_num, donor_addr):
-    # Define the recipient address     
-    recipient_Address = ''
-    if(int(rec_num) > 0 and int(rec_num) <= len(web3.eth.accounts)):
-        for idx in range(len(web3.eth.accounts)):
-            if(idx == (int(rec_num)-1)): recipient_Address = web3.eth.accounts[idx]
-        idx += 1 
-    else:
-        print('Wrong Recipient Account Selection.')            
-        exit()
-
-    # Input Recipient Amount
-    amounts = input("Enter your amounts of ethererum(ETH): ")        
-      
-    # get the nounce
-    nonce = web3.eth.getTransactionCount(donor_addr)
-
-    # build a transaction
-    tx = {
-        'nonce': nonce,
-        'to': recipient_Address,        
-        'value': web3.toWei(amounts, 'ether'),
-        'gas': 2000000,
-        'gasPrice': web3.toWei('50', 'gwei')
-    }
-
-    # sign a transaction
-    signed_tx = web3.eth.account.signTransaction(tx, _global_decrypted_key)
-
-    # send a transaction
-    tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
-
-    # decodes the transaction data    
-    print("\nTransaction Number:", web3.toHex(tx_hash))    
-    print("From:", web3.eth.getTransactionReceipt(tx_hash)['from'], ", To:", web3.eth.getTransactionReceipt(tx_hash)['to'])               
-    print("Transaction Amount:", web3.eth.getTransaction(tx_hash)['value'], "Wei (", toEther(web3.eth.getTransaction(tx_hash)['value']),"ETH =", toTransUSD(toEther(web3.eth.getTransaction(tx_hash)['value'])),"$USD) , GasPrice:", web3.eth.getTransaction(tx_hash)['gasPrice'])
-    print("GasUsed:", web3.eth.getTransactionReceipt(tx_hash)['gasUsed'])
-    print(web3.eth.getTransactionReceipt(tx_hash)['status']==1 and "The transaction was successful." or "The transaction was reverted by EVM.")
-
-    # Function to transfer web ethereum
+# Function to transfer web ethereum
 def sendWebEther(reci_addr, donor_addr, amounts):    
           
     # get the nounce
@@ -231,37 +155,43 @@ def sendWebEther(reci_addr, donor_addr, amounts):
     }
     
     # sign a transaction    
-    signed_tx = web3.eth.account.signTransaction(tx, extractPrincipalCipher(donor_addr))
-
-    print('reci_addr:', reci_addr)
-    print('donor_addr:', donor_addr)
-    print('nonce:', nonce)
-    print('PrincipalCipher:', extractPrincipalCipher(donor_addr))
-    print('signed_tx:', signed_tx)
+    signed_tx = web3.eth.account.signTransaction(tx, extractPrincipalCipher(donor_addr))    
 
     # send a transaction
     tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
 
-    # decodes the transaction data    
-    print("\nTransaction Number:", web3.toHex(tx_hash))    
-    print("From:", web3.eth.getTransactionReceipt(tx_hash)['from'], ", To:", web3.eth.getTransactionReceipt(tx_hash)['to'])               
-    print("Transaction Amount:", web3.eth.getTransaction(tx_hash)['value'], "Wei (", toEther(web3.eth.getTransaction(tx_hash)['value']),"ETH =", toTransUSD(toEther(web3.eth.getTransaction(tx_hash)['value'])),"$USD) , GasPrice:", web3.eth.getTransaction(tx_hash)['gasPrice'])
-    print("GasUsed:", web3.eth.getTransactionReceipt(tx_hash)['gasUsed'])
-    print(web3.eth.getTransactionReceipt(tx_hash)['status']==1 and "The transaction was successful." or "The transaction was reverted by EVM.")
+    # decodes the transaction data 
+    Tx_Status = web3.eth.getTransactionReceipt(tx_hash)['status']
+    Tx_Num = web3.toHex(tx_hash)
+    Frome = web3.eth.getTransactionReceipt(tx_hash)['from']
+    To = web3.eth.getTransactionReceipt(tx_hash)['to']
+    Wei_Amount = web3.eth.getTransaction(tx_hash)['value']
+    Eth_Amount = toEther(web3.eth.getTransaction(tx_hash)['value'])
+    Usd_Amount = toTransUSD(toEther(web3.eth.getTransaction(tx_hash)['value']))
+    Gas_Fees = web3.eth.getTransaction(tx_hash)['gasPrice']
+    Gas_Used = web3.eth.getTransactionReceipt(tx_hash)['gasUsed']
+    # print(web3.eth.getTransactionReceipt(tx_hash)['status']==1 and "The transaction was successful." or "The transaction was reverted by EVM.")
+    # print("\nTransaction Number:", web3.toHex(tx_hash))    
+    # print("From:", web3.eth.getTransactionReceipt(tx_hash)['from'], ", To:", web3.eth.getTransactionReceipt(tx_hash)['to'])               
+    # print("Transaction Amount:", web3.eth.getTransaction(tx_hash)['value'], "Wei (", toEther(web3.eth.getTransaction(tx_hash)['value']),"ETH =", toTransUSD(toEther(web3.eth.getTransaction(tx_hash)['value'])),"$USD) , GasPrice:", web3.eth.getTransaction(tx_hash)['gasPrice'])
+    # print("GasUsed:", web3.eth.getTransactionReceipt(tx_hash)['gasUsed'])    
+
+    return Tx_Status, Tx_Num, Frome, To, Wei_Amount, Eth_Amount, Usd_Amount, Gas_Fees, Gas_Used
+
+
+# Function of Tx results data 
+def txResultData(tx_result):
+    if(tx_result[0] == 1):              
+        message = Markup(f'The transaction was successful.<br>Transaction Number: {tx_result[1]}<br>From: {tx_result[2]}, To: {tx_result[3]}<br>Transaction Amount: {tx_result[4]} Wei ({tx_result[5]} ETH = {tx_result[6]} $USD)<br>GasPrice: {tx_result[7]}, GasUsed: {tx_result[8]}')        
+        flash(message, 'results') 
+    else:
+        message = "The transaction was failed and reverted by EVM."
+        flash(message, 'results') 
 
 
 # Connection Verification
 print("Established_Connections :", web3.isConnected())
 print("Current_Block # :", web3.eth.blockNumber, "\n")
-# # List Accounts Info Display
-# extractAccounts()
-# selectPrincipalAccount()
-# listAccounts()
-# # Input Recipient Address
-# rec_num = input("\nSelect your recipient address[#]: ")
-# # Transfer Ether Function
-# sendEther(rec_num, _global_principal_address)
-
 
 # Flask http web display
 app = Flask(__name__)
@@ -272,7 +202,7 @@ app.config['SECRET_KEY'] = '12345'
 def index():    
     account_name = extractAccounts()
     dataLen = len(account_name[0])    
-    print(account_name)        
+    # print(account_name)        
     return render_template('index.html', value0=account_name, value1=dataLen)    
         
 @app.route('/selectPrincipalData', methods=['POST'])
@@ -294,9 +224,8 @@ def selectRecipientInput():
 @app.route('/transferEther', methods=['POST'])
 def etherTransaction():    
     global _global_principal_address, _global_recipient_address        
-    etherAmount = request.form['inputEtherValue']
-    print(_global_recipient_address, _global_principal_address, etherAmount)
-    sendWebEther(_global_recipient_address, _global_principal_address, etherAmount)
+    etherAmount = request.form['inputEtherValue']    
+    txResultData(sendWebEther(_global_recipient_address, _global_principal_address, etherAmount))       
     # print("Selected Recipient Data :", recipientAddress)    
     # return render_template('ether_display.html', value0=principalAddress, value1=recipientAddress)
     return redirect(url_for('index'))
