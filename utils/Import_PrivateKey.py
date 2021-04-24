48ba8f33fea25a598dacc2bfecbbc6869119b0ea
Sunny Yoo <isunyoo@gmail.com>
2021-04-24 20:48:02 +0800

    Updated

--- utils/Import_PrivateKey.py
+++ utils/Import_PrivateKey.py
@@ -1,4 +1,4 @@
-import json, subprocess
+import json, subprocess, os, glob
 import pandas as pd
 from decouple import config
 
@@ -8,11 +8,26 @@ KEY_BASE = config('KEYSTORE_BASE')
 PRIVATE_KEY = config('PRIVATE_KEY_FILE')
 
 # Function to attach account in ETHEREUM_HOME
-def importPrivateKey(private_key):        
+def importPrivateKey(private_key): 
+    pfile = open(KEY_BASE+'/temp/passwdkey', 'w')
+    pfile.write(P_KEY)
+    pfile.close()
+
+    # save_path = '/home'
+    # file_name = "test.txt"
+    # completeName = os.path.join(save_path, file_name)
+    # print(completeName)
+
     with open(PRIVATE_KEY, 'w') as outfile:        
         outfile.write(json.dumps(private_key).replace('"', ''))    
-        status = subprocess.Popen(['geth', 'account', 'import', '--datadir', KEY_BASE, '--password', P_KEY, PRIVATE_KEY], text=True, stdout=subprocess.PIPE)        
+        status = subprocess.Popen(['geth', 'account', 'import', '--datadir', KEY_BASE, '--password', pfile, PRIVATE_KEY], text=True, stdout=subprocess.PIPE)        
         status.communicate()
+
+        # Delete all PrivateKey files        
+        # files = glob.glob(KEY_BASE+'/temp/*')
+        # for f in files:
+        #     os.remove(f)
+
     return status.returncode
 
     # # Attach accounts from json key file
