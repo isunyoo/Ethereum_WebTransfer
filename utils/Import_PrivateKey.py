bc96fc60d03ac2cc483e44ac6764d8d09c02c66d
isunyoo <isunyoo@gmail.com>
2021-04-25 15:25:14 +0800

    Updated for imported functions

--- utils/Import_PrivateKey.py
+++ utils/Import_PrivateKey.py
@@ -1,6 +1,6 @@
-import json, subprocess, os, glob
 import pandas as pd
 from decouple import config
+import json, subprocess, os, glob
 
 # Global variables
 P_KEY = config('KEY')
@@ -9,36 +9,20 @@ PRIVATE_KEY = config('PRIVATE_KEY_FILE')
 
 # Function to attach account in ETHEREUM_HOME
 def importPrivateKey(private_key): 
+    # Keyphrase file
     pfile = open(KEY_BASE+'/temp/passwdkey', 'w')
     pfile.write(P_KEY)
-    pfile.close()
+    pfile.close()    
 
-    # save_path = '/home'
-    # file_name = "test.txt"
-    # completeName = os.path.join(save_path, file_name)
-    # print(completeName)
-
+    # PrivateKey file
     with open(PRIVATE_KEY, 'w') as outfile:        
         outfile.write(json.dumps(private_key).replace('"', ''))    
-        status = subprocess.Popen(['geth', 'account', 'import', '--datadir', KEY_BASE, '--password', pfile, PRIVATE_KEY], text=True, stdout=subprocess.PIPE)        
+        status = subprocess.Popen(['geth', 'account', 'import', '--datadir', KEY_BASE, '--password', pfile.name, PRIVATE_KEY], text=True, stdout=subprocess.PIPE)        
         status.communicate()
 
-        # Delete all PrivateKey files        
-        # files = glob.glob(KEY_BASE+'/temp/*')
-        # for f in files:
-        #     os.remove(f)
+    # Delete all Key files        
+    files = glob.glob(KEY_BASE+'/temp/*')
+    for f in files:
+        os.remove(f)
 
     return status.returncode
-
-    # # Attach accounts from json key file
-    # df = pd.read_json(PRIVATE_KEY, typ='private_keys')
-    # # print(df.to_string())    
-    # i = 0
-    # while i < len(df.index):
-    #     with open(PRIVATE_KEY, 'w+') as pf:            
-    #         print("Private_Key :", df.iloc[i])            
-    #         pf.write(df.iloc[i])                               
-    #         # subprocess.Popen(['geth', 'account', 'import', '--datadir', KEY_BASE, PRIVATE_KEY], text=True, stdout=subprocess.PIPE)
-    #         subprocess.Popen(['geth', 'account', 'import', '--datadir', KEY_BASE, '--password', P_KEY, PRIVATE_KEY], text=True, stdout=subprocess.PIPE)
-    #         i += 1 
-
